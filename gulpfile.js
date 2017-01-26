f27da34b10729c27c717935bf7a1232f96fcf5b2
Justin <harwoodjp@g.cofc.edu>
2017-01-26 09:43:33 -0500

    init

---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+var gulp = require('gulp');
+var cleanCSS = require('gulp-clean-css');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
+var nodemon = require('gulp-nodemon');
+
+gulp.task('sass', function(){
+  return gulp.src('public/stylesheets/src/*.scss')
+    .pipe(sass()) // Converts Sass to CSS with gulp-sass
+    .pipe(gulp.dest('public/stylesheets/dist'))
+});
+
+gulp.task('autoprefix-then-minify', function(){
+  return gulp.src('public/stylesheets/dist/*.css')
+    .pipe(autoprefixer({
+        browsers: ['last 2 versions', '> 0%','Firefox ESR'],
+        cascade: false
+    }))
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('public/stylesheets/dist'))
+});
+
+gulp.task('nodemon', function() {
+    nodemon({
+        script: 'bin/www'
+    })
+    .on('restart', function onRestart() {
+      // reload connected browsers after a slight delay
+      setTimeout(function reload() {
+        browserSync.reload({
+          stream: false
+        });
+      }, BROWSER_SYNC_RELOAD_DELAY);
+    });
+
+})
+
+gulp.task('browser-sync', ['sass'], function() {
+    browserSync.init({
+        proxy: 'http://localhost:3000',
+        port: 4000,
+        online: true
+    });
+});
+
+gulp.task('default', ['browser-sync'], function() {
+    gulp.start('nodemon');    
+	gulp.watch('public/stylesheets/src/*.scss', ['sass']);
+	gulp.watch('public/stylesheets/dist/*.css', ['autoprefix-then-minify']);
+    gulp.watch("**/*.html").on('change', browserSync.reload);
+    gulp.watch("public/**/*").on('change', browserSync.reload);
+    gulp.watch("views/**/*").on('change', browserSync.reload);
+    gulp.watch("views/*.pug").on('change', browserSync.reload);
+
+});
